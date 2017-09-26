21fb37f71a01d8bca0c6fd717f73a74c5f0991fd
Antonio Torres <thetonio96@gmail.com>
2017-09-26 15:38:38 -0500

    Fixed verification rules

--- webserver.py
+++ webserver.py
@@ -12,13 +12,13 @@ app = Flask(__name__)
 @app.route('/', methods = ['POST'])
 def root():
     content = request.get_json()
-    if('command' in content):
+    if(not 'command' in content):
         return 'Missing {command}', 500
-    if('stateID' in content):
+    if(not 'stateID' in content):
         return 'Missing {stateID}', 500
-    if('robotID' in content):
+    if(not 'robotID' in content):
         return 'Missing {robotID}', 500
-    if('payload' in content):
+    if(not 'payload' in content):
         return 'Missing {payload}', 500
 
     return jsonify(content), 200

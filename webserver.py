aca018cf276bb56d753fa1786e1656d6bb06c607
Antonio Torres <thetonio96@gmail.com>
2017-09-28 12:02:48 -0500

    Testing GET POST

--- webserver.py
+++ webserver.py
@@ -22,20 +22,22 @@ app = Flask(__name__)
 #     strdata = data['command'] + "," + data['stateid'] + "," + data['robotid'] + "," + data['payload']
 #     return strdata
 
-@app.route('/', methods = ['POST'])
+@app.route('/', methods = ['POST', 'GET'])
 def root():
     content = request.get_json()
-    if(not 'command' in content):
-        return 'Missing {command}', 500
-    elif(not 'stateid' in content):
-        return 'Missing {stateid}', 500
-    elif(not 'robotid' in content):
-        return 'Missing {robotid}', 500
-    elif(not 'payload' in content):
-        return 'Missing {payload}', 500
+    print(content)
+    return jsonify(content), 200
+    # if(not 'command' in content):
+    #     return 'Missing {command}', 500
+    # elif(not 'stateid' in content):
+    #     return 'Missing {stateid}', 500
+    # elif(not 'robotid' in content):
+    #     return 'Missing {robotid}', 500
+    # elif(not 'payload' in content):
+    #     return 'Missing {payload}', 500
 
-    result, mid = mqttc.publish("robot/"+content['robotid'], content)
-    if result == paho.MQTT_ERR_SUCCESS:
-        return jsonify(content), 200
+    # result, mid = mqttc.publish("robot/"+content['robotid'], content)
+    # if result == paho.MQTT_ERR_SUCCESS:
+    #     return jsonify(content), 200
     
-    return "WIERD ERROR", 500
+    # return "WIERD ERROR", 500

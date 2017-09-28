292f700e96fdf4dd9a09e9ba1e94cca3588bb09d
Antonio Torres <thetonio96@gmail.com>
2017-09-28 12:27:30 -0500

    Server JSON

--- webserver.py
+++ webserver.py
@@ -26,20 +26,20 @@ app = Flask(__name__)
 def root():
 
     content = request.get_json()
-        if(not 'command' in content):
-            return 'Missing {command}', 500
-        elif(not 'stateid' in content):
-            return 'Missing {stateid}', 500
-        elif(not 'robotid' in content):
-            return 'Missing {robotid}', 500
-        elif(not 'payload' in content):
-            return 'Missing {payload}', 500
+    if(not 'command' in content):
+        return 'Missing {command}', 500
+    elif(not 'stateid' in content):
+        return 'Missing {stateid}', 500
+    elif(not 'robotid' in content):
+        return 'Missing {robotid}', 500
+    elif(not 'payload' in content):
+        return 'Missing {payload}', 500
 
-        result, mid = mqttc.publish("robot/"+content['robotid'], content)
-        if result == paho.MQTT_ERR_SUCCESS:
-            return jsonify(content), 200
-        
-        return "WIERD ERROR", 500
+    result, mid = mqttc.publish("robot/"+content['robotid'], content)
+    if result == paho.MQTT_ERR_SUCCESS:
+        return jsonify(content), 200
+    
+    return "WIERD ERROR", 500
     # try:
         
     # except:
